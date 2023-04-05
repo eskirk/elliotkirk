d0665879d86d35515c4ad7159bd6331f406312ea
Elliot Kirk <elliot.kirk@grafana.com>
2023-04-05 16:06:25 -0700

    Updating collector

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { initializeFaro } from '@grafana/faro-web-sdk';
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 import { App } from './App';
 import './style.scss'
 
 initializeFaro({
-  url: 'https://faro-collector-prod-us-central-0.grafana.net/collect/8ebb309a2a2a14a8f3d81a0d9e9d3a9f',
+  url: 'https://faro-collector-ops-us-east-0.grafana-ops.net/collect/53a38142e55b4beec237143a2140ca51',
   app: {
     name: 'eskirk.github.io',
     version: '1.0.0',
     environment: 'production'
   },
+  instrumentations: [
+    // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
+    ...getWebInstrumentations(),
+    // Mandatory, initialization of the tracing package
+    new TracingInstrumentation(),
+  ],
 });
 
 // Create a root.
